d5caefd2f1a1ea9da7e41c3020dc2230521149e5
ash <wiredash@gmail.com>
2018-09-03 22:00:10 -0400

    first update
    Basic changes - more needed

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express')
+//var env = require('./env.js')
 var app = express()
 var hogan = require('hogan-express')
 app.engine('html', hogan)
 app.set('port', (process.env.PORT || 3000))
 app.use('/', express.static(__dirname + '/public/'))
 var Cosmic = require('cosmicjs')
-var bucket_slug = process.env.COSMIC_BUCKET || 'stellar'
-var read_key = process.env.COSMIC_READ_KEY
+var bucket_slug = process.env.COSMIC_BUCKET || 'ab02'
+var read_key = process.env.COSMIC_READ_KEY || 'Dx3EzzXQxyUWTgOBQh9Y6NCrzzJM7H4TiBMi6sda3NEXRF94MH'
 app.get('/', function(req, res) {
   Cosmic.getObjects({ bucket: { slug: bucket_slug, read_key: read_key } }, function(err, response) {
     res.locals.cosmic = response;
---
+++ env.js
@@ -0,0 +1,2 @@
+process.env['COSMIC_BUCKET'] = 's1'
+process.env['COSMIC_READ_KEY'] = 'ez2tRIIi2AfOkPNqksE9CGGrCSwIGSwA6NA3hkLjGXlZLOaIBu'
